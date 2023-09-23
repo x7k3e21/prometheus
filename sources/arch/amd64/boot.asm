3986ba878c13da20dffe468ecd1fb8c2bc407c63
x7k3e21 <uly4nkin.denis@gmail.com>
2023-09-23 21:37:55 +0300

    Added GDT32, added code for entering Protected mode

--- sources/arch/amd64/boot.asm
+++ sources/arch/amd64/boot.asm
@@ -15,11 +15,14 @@ _bootloader_entry:
 
     call clear.start
 
-    mov si, greeting_message
+    cli
+    lgdt [GDT32.descriptor]
+    
+    mov eax, cr0
+    or  eax, 1
+    mov cr0, eax
 
-    call print.start
-
-    jmp $
+    jmp GDT32.code:_pm_bootloader_entry
 
 clear.start:
     mov ah, 0x06
@@ -63,7 +66,79 @@ print.done:
 
 greeting_message db "Hello, world!", 0
 
+GDT32.start:
+    dq 0x0000000000000000
+GDT32.code: equ $ - GDT32.start
+    db 0xFF, 0xFF
+    db 0x00, 0x00, 0x00
+
+    db 10011010b
+    db 11001111b
+
+    db 0x00
+GDT32.data: equ $ - GDT32.start
+    db 0xFF, 0xFF
+    db 0x00, 0x00, 0x00
+
+    db 10010010b
+    db 11001111b
+
+    db 0x00
+GDT32.end:
+
+GDT32.descriptor:
+    dw GDT32.end - GDT32.start - 1
+    dd GDT32.start
+
+    [bits 32]
+
+_pm_bootloader_entry:
+    mov ax, GDT32.data
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    mov esp, stack.top
+
+    mov ebx, greeting_message
+
+    call _print_message.begin
+
+    jmp $
+
+%define VIDEO_MEMORY_ADDRESS 0xB8000
+
+_print_message.begin:
+    pusha
+    mov edx, VIDEO_MEMORY_ADDRESS
+
+_print_message.loop:
+    mov al, [ebx]
+    mov ah, 0x0F
+
+    cmp al, 0
+    je _print_message.end
+
+    mov [edx], ax
+    add ebx, 1
+    add edx, 2 
+
+    jmp _print_message.loop
+
+_print_message.end:
+    popa
+
+    ret
+
     times 510 - ($ -$$) db 0
 
     db 0x55
     db 0xAA
+
+    section .bss
+
+stack.bottom:
+    resb 16 * 1024
+stack.top:
